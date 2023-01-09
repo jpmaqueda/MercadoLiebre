507f28a0374a80e0f96b5e39f080b6dbaf06d480
jpmaqueda <jpmaqueda@gmail.com>
2023-01-09 21:37:30 +0000

    me dijeron que no putee mas

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const path = require(`path`);
 const daemon = require(`daemon`);
 
 // ESTO ES MUCHO MUY IMPORTANTE PORQUE SI NO NO VEO LAS IMAGENES
-const publicpath = path.join(__dirname, `./`);
+const publicpath = path.join(__dirname, `./Public`);
 app.use(express.static(publicpath));
 //ESTO ES UN DIVISOR
 
-const port = process.env.port || 3005;
-app.listen(port, ()=> console.log(`servidor corriendo en el puerto ${port}`));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log(`servidor corriendo en el puerto ${port}`));
 
 app.get(`/`, (req, res) => {
   let htmlpath = path.join(__dirname, `./views/home.html`);
